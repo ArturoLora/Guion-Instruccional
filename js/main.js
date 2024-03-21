8e12fd93f8db626bdf0d197178fe8e6122fd213f
Arturo Lora <81186953+ArturoLora@users.noreply.github.com>
2024-03-21 15:55:04 -0700

    Update main.js
    update para que el active funcione en github pages

--- js/main.js
+++ js/main.js
@@ -55,18 +55,22 @@ class HeaderComponent extends HTMLElement {
   
   customElements.define('footer-component', FooterComponent);
 
-  document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function() {
     const currentPage = window.location.pathname.split("/").pop();
 
     const navLinks = document.querySelectorAll(".navbar-nav .nav-link");
   
     navLinks.forEach(function(navLink) {
-      const linkHref = navLink.getAttribute("href").split("/").pop(); 
+        let linkHref = navLink.getAttribute("href").split("/").pop(); 
+        if (!linkHref || linkHref === "index.html") {
+            linkHref = "index.html";
+        }
   
-      if (linkHref === currentPage) {
-        navLink.classList.remove("unidad"); 
-        navLink.classList.add("active"); 
-      }
+        if (linkHref === currentPage) {
+            navLink.classList.remove("unidad"); 
+            navLink.classList.add("active"); 
+        }
     });
-  });
+});
+
   
